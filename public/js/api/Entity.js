b13673cdc9182812790c82c38f400b41a47f07a0
Ivan Khokhlov <mozhno@MacBook-Air-Mozhno.local>
2023-07-07 19:36:40 +0300

    Initial commit

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -9,7 +9,7 @@ class Entity {
    * (в зависимости от того, что наследуется от Entity)
    * */
   static list(data, callback){
-
+  
   }
 
   /**
--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,6 +2,39 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {
 
-};
+
+const createRequest = (options = {}) => {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    let method = '';
+    let url = options.url;
+
+    if (options.method === 'GET') {
+        const urlForm = `${options.url}?mail=${options.data.mail}&password=${options.data.password}`;
+        method = 'GET';
+        url = urlForm;
+    }
+
+    if (options.method !== 'GET') {
+        const formData = new FormData();
+        method = options.method;
+        formData.append('mail', options.data.mail);
+        formData.append('password', options.data.password);
+    }
+
+    try {
+        xhr.open(method, url);
+        xhr.send(formData);
+        
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+                options.callback = (xhr.response.error, xhr.response);
+            }
+        }
+    }
+    catch (e) {
+        console.log(e);
+    }
+
+}
